d6c8bda1431bfd8bcd518ced9b9ffeffee033977
Jorge Morales R <jorgeluismreyes@gmail.com>
2025-09-16 20:50:37 -0600

    Crud de libros

---
+++ database/migrations/2025_09_16_225859_create_libros_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->bigIncrements('libro_id');
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('isbn')->unique();
+            $table->year('anio_publicacion')->nullable();
+            $table->string('editorial')->nullable();
+            $table->unsignedInteger('cantidad_ejemplares')->default(0);
+            $table->unsignedInteger('ejemplares_disponible')->default(0);
+            $table->enum('estado', ['disponible', 'agotado', 'inactivo'])->default('disponible');
+            $table->string('imagen_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('libros');
+    }
+};
